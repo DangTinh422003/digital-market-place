6ffe04600bc0569877c82b00a376d4fcabef2cb2
dangtinh <caodangtinh04022003@gmail.com>
2024-07-22 16:28:34 +0700

    add: setup db and ORM

---
+++ src/app/api/auth/creation/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id || user === null) {
+    throw new Error("Something went wrong...");
+  }
+
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    const newUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name || "",
+        lastName: user.family_name || "",
+        email: user.email || "",
+        profileImage:
+          user.picture || `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return NextResponse.redirect("http://localhost:3000");
+}
