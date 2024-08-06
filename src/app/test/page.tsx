8143b895f1948c08e7ae36b924454a6d00709afd
dangtinh <caodangtinh04022003@gmail.com>
2024-08-06 22:58:38 +0700

    update: server action

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia
+      voluptates aliquid commodi repellendus dolorum nesciunt pariatur
+      doloremque deleniti praesentium assumenda velit, et quibusdam molestiae
+      eligendi voluptas quos neque accusantium?
+    </div>
+  );
+};
+
+export default page;
